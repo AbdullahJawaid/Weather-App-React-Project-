7d5c78060f3c8fbe8d97fe01785a538ab3598b44
AbdullahJawaid <91978361+AbdullahJawaid@users.noreply.github.com>
2023-05-12 23:07:43 +0500

    first commit

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Weather from './component/Weather';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Weather/>
     </div>
   );
 }
---
+++ weather-app/src/component/Weather.js
@@ -0,0 +1,15 @@
+// https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=30c2da51076e9cae437aec5923654ec0
+
+import React from 'react'
+
+import './Weather.css '
+
+function Weather() {
+  return (
+    <>
+      Hello
+    </>
+  )
+}
+
+export default Weather
